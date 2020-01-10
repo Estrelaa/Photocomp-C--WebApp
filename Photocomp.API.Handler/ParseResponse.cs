31a9268df195c140e139169952f86ed1ca8ad16f
Kieran Winfield <K.Winfield@ucas.ac.uk>
2020-01-10 12:27:34 +0000

    Working on backend changes

---
+++ Photocomp.API.Handler/ParseResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Photocomp.API.Handler
+{
+    class ParseResponse
+    {
+        public void ParseJson()
+        {
+
+        }
+    }
+}
